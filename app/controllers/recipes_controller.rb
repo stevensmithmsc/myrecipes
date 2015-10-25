4dbfe3d9de428d2286ceab854c5291cb4f41a485
stevensmith <stevensmithmsc@gmail.com>
2015-10-25 21:50:36 +0000

    add pagination

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,8 @@
 class RecipesController < ApplicationController
   
   def index
-    @recipes = Recipe.all
+#    @recipes = Recipe.all.sort_by{|likes| likes.thumbs_up_total}.reverse
+    @recipes = Recipe.paginate(page: params[:page], per_page: 4)
   end
   
   def show
@@ -38,6 +39,18 @@ class RecipesController < ApplicationController
       render :edit
     end
     
+  end
+  
+  def like
+    @recipe = Recipe.find(params[:id])
+    like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+    if like.valid?
+      flash[:success] = "Your selection was succesful"
+      redirect_to :back
+    else
+      flash[:danger] = "You can only like/dislike a recipe once"
+      redirect_to :back
+    end
   end
   
   private 
