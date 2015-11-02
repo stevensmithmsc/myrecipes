04659c3d293033fd9e9fde3906880f02e64d4ee8
stevensmith <stevensmithmsc@gmail.com>
2015-11-02 22:08:01 +0000

    create chefs resources and add authentication

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,7 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:edit, :update, :show, :like]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update]
   
   def index
 #    @recipes = Recipe.all.sort_by{|likes| likes.thumbs_up_total}.reverse
@@ -6,7 +9,7 @@ class RecipesController < ApplicationController
   end
   
   def show
-    @recipe = Recipe.find(params[:id])
+    
   end
   
   def new
@@ -15,7 +18,7 @@ class RecipesController < ApplicationController
   
   def create 
     @recipe = Recipe.new(recipe_params)
-    @recipe.chef = Chef.find(2)
+    @recipe.chef = current_user
     
     if @recipe.save
       flash[:success] = "Your recipe was created succesfully!"
@@ -26,13 +29,11 @@ class RecipesController < ApplicationController
   end
   
   def edit
-    @recipe = Recipe.find(params[:id])
+    
   end
   
   def update
-    @recipe = Recipe.find(params[:id])
     if @recipe.update(recipe_params)
-      #do something
       flash[:success] = "Your recipe was updated succesfully!"
       redirect_to recipe_path(@recipe)
     else
@@ -42,8 +43,7 @@ class RecipesController < ApplicationController
   end
   
   def like
-    @recipe = Recipe.find(params[:id])
-    like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+    like = Like.create(like: params[:like], chef: current_user, recipe: @recipe)
     if like.valid?
       flash[:success] = "Your selection was succesful"
       redirect_to :back
@@ -58,5 +58,15 @@ class RecipesController < ApplicationController
       params.require(:recipe).permit(:name, :summary, :description, :picture)
     end
 
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+    
+    def require_same_user
+      if current_user != @recipe.chef
+        flash[:danger] = "You can only edit your own recipes"
+        redirect_to recipes_path
+      end
+    end
   
 end
